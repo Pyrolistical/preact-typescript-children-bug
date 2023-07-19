fc92ee8aea61f03a9ba0c061b5a2eaddd372ce00
Ronald Chen <pyrogx1133@gmail.com>
2023-07-18 20:29:46 -0700

    removed attempt to fix with functional component typing

--- index.tsx
+++ index.tsx
@@ -1,12 +1,12 @@
-import { render, createContext, FunctionalComponent } from "preact";
+import { render, createContext } from "preact";
 import { useContext } from "preact/hooks";
 
 const SomeContext = createContext<string>(undefined);
 
-const App: FunctionalComponent<{}> = () => {
+function App() {
   const contextValue = useContext(SomeContext);
   return <h1>{contextValue}</h1>;
-};
+}
 
 render(
   <SomeContext.Provider value={"Hello"}>
